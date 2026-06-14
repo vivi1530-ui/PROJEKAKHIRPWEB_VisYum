27f7641a4f953638a3d769e23e908f4db0fa718d
Adine Vivia <adinevivia15@gmail.com>
2026-06-14 20:23:14 +0700

    fix: force https connection on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Menambahkan library URL untuk handle HTTPS
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Memaksa Laravel menggunakan HTTPS jika aplikasi berjalan di Railway (Production)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
